f5e5b929b0957b430c9c369f08bb1fd39a9b8cb4
KelvinCastle13 <kelvincastle13@gmail.com>
2025-07-27 20:51:44 -0400

    got the site to work with connected api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { BookService, Book } from './service/book.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'book-tracker-frontend';
+
+export class AppComponent implements OnInit {
+  books: Book[] = [];
+  newBook: Partial<Book> = {};
+  editBook: Book | null = null;
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.bookService.getBooks().subscribe(data => this.books = data);
+  }
+
+  createBook() {
+    if (this.newBook.title && this.newBook.author && this.newBook.genre) {
+      this.bookService.addBook(this.newBook as Book).subscribe(() => {
+        this.newBook = {};
+        this.loadBooks();
+      });
+    }
+  }
+
+  startEdit(book: Book) {
+    this.editBook = { ...book };
+  }
+
+  cancelEdit() {
+    this.editBook = null;
+  }
+
+  saveEdit() {
+    if (this.editBook) {
+      this.bookService.updateBook(this.editBook.id, this.editBook).subscribe(() => {
+        this.editBook = null;
+        this.loadBooks();
+      });
+    }
+  }
+
+  deleteBook(id: number) {
+    this.bookService.deleteBook(id).subscribe(() => this.loadBooks());
+  }
 }
---
+++ src/app/service/book.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Book {
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class BookService {
+  private apiUrl = 'http://localhost:8000'
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.apiUrl}/`);
+  }
+
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(`${this.apiUrl}/books/`, book);
+  }
+
+  getBook(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/books/${id}`);
+  }
+
+  updateBook(id: number, book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.apiUrl}/books/${id}`, book);
+  }
+
+  deleteBook(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/books/${id}`);
+  }
+}
